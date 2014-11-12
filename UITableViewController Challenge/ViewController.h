b4f1ac05ff0c6f634d29e48b39a3055979d6da9b
Alex <office@cognitivebits.com>
2014-11-12 12:45:22 +0200

    Initial Commit

---
+++ UITableViewController Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge
+//
+//  Created by Alex on 11/12/14.
+//  Copyright (c) 2014 Tedrasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewController Challenge/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge
+//
+//  Created by Alex on 11/12/14.
+//  Copyright (c) 2014 Tedrasoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
